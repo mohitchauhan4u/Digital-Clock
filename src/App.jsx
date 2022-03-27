08a5f7aba275293b7e2f3733d839d2a8b1dc65ba
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-27 19:23:26 +0530

    added documentation and final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+/**
+ * It updates the time every second.
+ * @returns A function that returns a string.
+ */
 import React, { useState } from "react";
 import './App.css'
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* It's importing the React library, the ReactDOM library, and the App component. */
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App'
